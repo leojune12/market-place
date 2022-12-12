a482e7cfdb55fc7dce6f03a28d65fc0450c769aa
Leo June <leojunebedeo@gmail.com>
2022-12-12 18:45:35 +0800

    Update AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -27,7 +27,7 @@
 
         <div class="w-full">
             <div class="flex flex-col">
-                <Disclosure as="nav" class="md:h-16 border-b" v-slot="{ open }">
+                <Disclosure as="nav" class="md:h-16 border-b bg-white" v-slot="{ open }">
                     <div class="px-2 sm:px-6 lg:px-8">
                         <div class="relative flex h-16 items-center justify-between">
                             <div class="absolute inset-y-0 left-0 flex items-center md:hidden">
