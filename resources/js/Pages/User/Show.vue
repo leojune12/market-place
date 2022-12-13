e0af636ad43d8badc63ac8bc4aa8070d8a414e0a
Leo June <leojunebedeo@gmail.com>
2022-12-14 00:52:04 +0800

    Add users show

---
+++ resources/js/Pages/User/Show.vue
@@ -0,0 +1,76 @@
+<template>
+    <Head title="User" />
+
+    <AuthenticatedLayout>
+        <div class="overflow-hidden bg-white shadow sm:rounded-lg mb-5">
+            <div class="px-4 py-5 sm:px-6">
+                <h3 class="text-lg font-medium leading-6 text-gray-900">User Information</h3>
+                <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+            </div>
+            <div class="border-t border-gray-200">
+                <dl>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">First name</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ model.first_name }}</dd>
+                    </div>
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">Last name</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ model.last_name }}</dd>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">Email address</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ model.last_name }}</dd>
+                    </div>
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">Role</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 capitalize ">{{ model.roles[0].name }}</dd>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">Created At</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ model.date_added }}</dd>
+                    </div>
+                </dl>
+            </div>
+            <div class="flex flex-col md:flex-row gap-4 py-6 sm:px-6 px-4">
+                <LinkComponent
+                    :href="url"
+                    type="secondary"
+                >
+                    Back
+                </LinkComponent>
+                <LinkComponent
+                :href="url + '/' + model.id + '/edit'"
+                    type="primary"
+                >
+                    Edit
+                </LinkComponent>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script>
+    import { PaperClipIcon } from '@heroicons/vue/20/solid'
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+    import { Head, Link } from '@inertiajs/inertia-vue3'
+    import LinkComponent from '@/Components/LinkComponent.vue';
+
+    export default {
+        components: {
+            PaperClipIcon,
+            AuthenticatedLayout,
+            Head,
+            Link,
+            LinkComponent,
+        },
+        data() {
+            return {
+                url: '/users'
+            }
+        },
+        props: {
+            model: Object,
+            roles: Array,
+        },
+    }
+</script>
