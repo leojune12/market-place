8ab18ffdf7e1ad3ce9f4c754a89fa3d60e154d29
Leo June <leojunebedeo@gmail.com>
2022-12-13 09:54:51 +0800

    Add user index

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -2,137 +2,111 @@
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
-        </template>
-
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="overflow-x-auto">
+                <div class="overflow-x-auto mb-4">
                     <table class="min-w-full">
-                        <thead class="border bg-slate-800 text-sm font-medium text-white">
+                        <thead class="border bg-slate-600 text-sm font-medium text-white">
                             <tr>
-                                <th class="py-4 px-6 text-left">
-                                    #
+                                <th class="py-3 px-6 text-left">
+                                    ID
                                 </th>
-                                <th class="py-4 px-6 text-left">
-                                    Heading
+                                <th class="py-3 px-6 text-left">
+                                    First Name
                                 </th>
-                                <th class="py-4 px-6 text-left">
-                                    Heading
+                                <th class="py-3 px-6 text-left">
+                                    Last Name
                                 </th>
-                                <th class="py-4 px-6 text-left">
-                                    Heading
+                                <th class="py-3 px-6 text-left">
+                                    Email
                                 </th>
-                                <th class="py-4 px-6 text-left">
-                                    Heading
-                                </th>
-                                <th class="py-4 px-6 text-left">
-                                    Heading
-                                </th>
-                                <th class="py-4 px-6 text-left">
-                                    Heading
-                                </th>
-                                <th class="py-4 px-6 text-left">
-                                    Heading
-                                </th>
-                                <th class="py-4 px-6 text-left">
-                                    Heading
+                                <th class="py-3 px-6 text-left">
+                                    Actions
                                 </th>
                             </tr>
                         </thead>
                         <tbody class="border">
-                            <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
+                            <tr
+                                v-for="item in response.data"
+                                :key="item.id"
+                                class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+                            >
+                                <td class="px-6 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    {{ item.id }}
                                 </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
+                                <td class="text-sm text-gray-900 font-light px-6 py-3 whitespace-nowrap">
+                                    {{ item.first_name }}
                                 </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
+                                <td class="text-sm text-gray-900 font-light px-6 py-3 whitespace-nowrap">
+                                    {{ item.last_name }}
                                 </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
+                                <td class="text-sm text-gray-900 font-light px-6 py-3 whitespace-nowrap">
+                                    {{ item.email }}
                                 </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
-                                </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    Cell
+                                <td class="text-sm text-gray-900 font-light px-6 py-3 whitespace-nowrap">
+                                    <div class="flex gap-2">
+                                        <Link
+                                            :href="'/users/' + item.id"
+                                            class="text-green-500 hover:text-green-600 transition duration-300 ease-in-out"
+                                            title="View"
+                                        >
+                                            <EyeIcon class="block h-5 w-5" aria-hidden="true" />
+                                        </Link>
+                                        <Link
+                                            :href="'/users/' + item.id + '/edit'"
+                                            class="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out"
+                                            title="Edit"
+                                        >
+                                            <PencilSquareIcon class="block h-5 w-5" aria-hidden="true" />
+                                        </Link>
+                                        <Link
+                                            href="#"
+                                            class="text-red-500 hover:text-red-600 transition duration-300 ease-in-out"
+                                            title="Delete"
+                                        >
+                                            <TrashIcon class="block h-5 w-5" aria-hidden="true" />
+                                        </Link>
+                                    </div>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
+                <PaginationComponent
+                    :totalPages="response.last_page"
+                    :perPage="response.per_page"
+                    :currentPage="response.current_page"
+                    :from="response.from"
+                    :to="response.to"
+                    :total="response.total"
+                    url="/users"
+                />
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
 
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+<script>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+    import PaginationComponent from '@/Components/Table/Pagination.vue'
+    import { Head, Link } from '@inertiajs/inertia-vue3'
+    import { EyeIcon, PencilSquareIcon, TrashIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/24/outline'
+
+    export default {
+        components: {
+            AuthenticatedLayout,
+            PaginationComponent,
+            Head,
+            Link,
+            EyeIcon,
+            PencilSquareIcon,
+            TrashIcon,
+            ChevronLeftIcon,
+            ChevronRightIcon,
+        },
+        props: {
+            response: Object,
+        },
+    }
 
 </script>
