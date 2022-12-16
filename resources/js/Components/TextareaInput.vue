c07c27f30a8228f0f48cb7af3db11a4d044dee6b
Leo June <leojunebedeo@gmail.com>
2022-12-16 23:31:49 +0800

    Add TextareaInput component

---
+++ resources/js/Components/TextareaInput.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+defineProps(['modelValue', 'rows', 'placeholder']);
+
+defineEmits(['update:modelValue']);
+
+const input = ref(null);
+
+onMounted(() => {
+    if (input.value.hasAttribute('autofocus')) {
+        input.value.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+    <!-- <input
+        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm h-10"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        ref="input"
+    /> -->
+    <textarea
+        id="about"
+        :rows="rows"
+        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        :placeholder="placeholder"
+        @input="$emit('update:modelValue', $event.target.value)"
+        ref="input"
+    >
+        {{ modelValue }}
+    </textarea>
+</template>
