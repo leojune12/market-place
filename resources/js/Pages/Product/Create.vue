1a99f878193437a59fe670bfba8af8072ffc640f
Leo June <leojunebedeo@gmail.com>
2022-12-18 22:51:39 +0800

    Add required

--- resources/js/Pages/Product/Create.vue
+++ resources/js/Pages/Product/Create.vue
@@ -24,7 +24,7 @@
                                 type="text"
                                 class="mt-1 block w-full"
                                 v-model="form.name"
-
+                                required
                                 autofocus
                                 autocomplete="name"
                             />
