8ebc6649f20185fb1e5de382660f6fbda28cbfae
Leo June <leojunebedeo@gmail.com>
2022-12-18 22:37:08 +0800

    Update AddressService

--- Modules/Address/Services/AddressService.php
+++ Modules/Address/Services/AddressService.php
@@ -14,14 +14,20 @@ class AddressService
     // Cities and Municipalities
     public static function getBarangaysPerCity($city_code)
     {
-        $barangays = Barangay::where('citymunCode', $city_code)->select('brgyCode', 'brgyDesc')->get();
+        $barangays = Barangay::where('citymunCode', $city_code)
+            ->select('brgyCode', 'brgyDesc')
+            ->orderBy('brgyDesc')
+            ->get();
 
         return $barangays;
     }
 
     public static function getCityPerProvince($province_code)
     {
-        $cities = City::where('provCode', $province_code)->select('citymunCode', 'citymunDesc')->get();
+        $cities = City::where('provCode', $province_code)
+            ->select('citymunCode', 'citymunDesc')
+            ->orderBy('citymunDesc')
+            ->get();
 
         return $cities;
     }
